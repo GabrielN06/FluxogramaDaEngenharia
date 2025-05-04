94c3956ab2d3f30d1aea26ce1bf232290abe28ab
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-05-04 20:43:20 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("--- Fluxograma da Engenharia ---");
+
+Console.Write("\nO objeto está se movendo (S/N)? ");
+string movendo = Console.ReadLine()!.ToUpper();
+
+Console.Write("Deveria? ");
+string deveria = Console.ReadLine()!.ToUpper();
+
+if(movendo == "N" && deveria == "S"){
+    Console.WriteLine("\nUse WD-40.");
+
+}
+else if (movendo == "S" && deveria =="N")
+  {
+      Console.WriteLine("\nUse Silver Tape.");
+}
+else if (movendo == "N" && deveria == "N"){
+    Console.WriteLine("\nÓtimo!");
+}
+else if(movendo == "S" && deveria == "S"){
+    Console.WriteLine("\nÓtimo!");
+}
